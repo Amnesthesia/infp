eb4d66e3c92548351f4141098af9e81dccf72ff6
Amnesthesia <sintpanda@gmail.com>
2015-02-15 20:16:30 +0100

    Precompile assets in production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.compile = true
+Rails.application.config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( foundation-social-web-icons.png )
 
