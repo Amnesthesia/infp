8a7524189d72466965d1e8da10fd7494fe0f1623
Amnesthesia <sintpanda@gmail.com>
2015-02-15 21:24:40 +0100

    Added missing column in user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215162408) do
+ActiveRecord::Schema.define(version: 20150215202426) do
 
   create_table "images", force: :cascade do |t|
     t.string   "path"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150215162408) do
     t.string   "last_sign_in_ip"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
